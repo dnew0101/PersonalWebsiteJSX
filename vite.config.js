a6fed2dc53fd7942e8f197601a17fada034d9d20
dnew0101 <david.newman.dev1@gmail.com>
2025-10-19 22:10:38 -0700

    vite.config an yaml update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  base: '/PersonalWebsiteJSX/',
   plugins: [
     react(),
     tailwindcss(),
@@ -10,6 +15,5 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-  },
-  base: '/PersonalWebsiteJSX',
+  }
 })
